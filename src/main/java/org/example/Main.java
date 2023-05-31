926bc39ebeeee9a333e24b325df9f1eb125da35f
Slava Rybkin <yolka.sd@gmail.com>
2023-05-31 13:19:39 +0700

    added singleton config

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     //private static final Configuration configuration = new Configuration("src/main/resources/config.properties");
     public static void main(String[] args) {
-        new Bot(args[0]);
+        SingletonConfiguration singletonConfiguration = SingletonConfiguration.getInstance();
+        new Bot(singletonConfiguration.getBotToken());
     }
 }
---
+++ src/main/java/org/example/SingletonConfiguration.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SingletonConfiguration {
+    private File configFile;
+    private final String botToken;
+    private SingletonConfiguration() throws IOException {
+        this.configFile = getFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Config config = mapper.readValue(this.configFile, new TypeReference<>() {});
+        botToken = config.getBotToken();
+    }
+
+    private static final class ConfigHolder{
+        private final static SingletonConfiguration instance;
+
+        static {
+            try {
+                instance = new SingletonConfiguration();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private File getFile() {
+        this.configFile = new File("src/main/resources/config.json");
+        if (!configFile.exists()) {
+            throw new RuntimeException("Config file not found");
+        }
+        return configFile;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public static SingletonConfiguration getInstance() {
+        return ConfigHolder.instance;
+    }
+    private static class Config {
+        private String botToken;
+
+        public Config() {
+
+        }
+
+        public String getBotToken() {
+            return botToken;
+        }
+
+        public void setBotToken(String botToken) {
+            this.botToken = botToken;
+        }
+    }
+
+}
+
+
