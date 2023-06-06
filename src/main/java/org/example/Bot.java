0713c0d3f1f6fce90e125383ec1c30b1ff332189
Slava Rybkin <yolka.sd@gmail.com>
2023-06-06 10:32:34 +0700

    MVP 1

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -24,13 +24,39 @@ public class Bot {
     private void handleUpdate(Update update) {
         String userText = extractUserText(update);
         long chatId = extractChatId(update);
+        long messageId = extractMessageId(update);
         if (userText != null) {
             if (userText.equals("/start")) {
                 sendResponse(chatId, "Bot activated");
-            } else if(userText.equals("/hangman")) {
-                hangman(userText, chatId);
-            } else if (userText.equals("/hangman_stop")) {
+            } else if (userText.equals("/hangman")) {
+                guessWordGame = new Game("src/main/resources/words.json");
+                sendResponse(chatId, "Игра началась. Я загадал слово - вы должны его отгадать!\n" + guessWordGame.getGuessedWord());
+            } else if (guessWordGame != null) {
+                String guessWord = guessWordGame.playGame(userText);
+                String word = guessWordGame.getWord();
 
+                if (guessWordGame.gameStatus()) {
+                    if (!userText.equals("/hangman_stop")) {
+                        if (userText.length() > 1) {
+                            if (word.equals(userText.toUpperCase())) {
+                                sendResponse(chatId, "Вы угадали целое слово:\n" + word, messageId);
+                            } else {
+                                sendResponse(chatId, "Увы, слово не верное", messageId);
+                            }
+                        } else {
+                            if (word.contains(userText.toUpperCase())) {
+                                sendResponse(chatId, "Вы угадали, такая буква есть в слове:\n" + guessWord, messageId);
+                            } else {
+                                sendResponse(chatId, "Увы, такой буквы нет", messageId);
+                            }
+                        }
+                    } else {
+                        guessWordGame = null;
+                    }
+                } else {
+                    sendResponse(chatId, "Вы победили\n" + guessWord);
+                    guessWordGame = null;
+                }
             }
         }
     }
@@ -49,11 +75,22 @@ public class Bot {
         return 0;
     }
 
+    private long extractMessageId(Update update) {
+        if (update.message() != null && update.message().chat() != null) {
+            return update.message().messageId();
+        }
+        return 0;
+    }
+
     private void sendResponse(long chatId, String message) {
         telegramBot.execute(new SendMessage(chatId, message));
     }
 
-    private void hangman(String userText, long chatId) {
+    private void sendResponse(long chatId, String message, long messageId) {
+        telegramBot.execute(new SendMessage(chatId, message).replyToMessageId((int) messageId));
+    }
+
+    private void hangman(String userText, long chatId, long messageId) {
         guessWordGame = new Game("src/main/resources/words.json");
         sendResponse(chatId, "Игра началась. Я загадал слово - вы должны его отгадать!\n" + guessWordGame.getGuessedWord());
         if (guessWordGame != null) {
@@ -79,7 +116,7 @@ public class Bot {
                     guessWordGame = null;
                 }
             } else {
-                sendResponse(chatId, "Вы победили\n" + guessWord);
+                sendResponse(chatId, "Вы победили\n" + guessWord, messageId);
                 guessWordGame = null;
             }
         }
